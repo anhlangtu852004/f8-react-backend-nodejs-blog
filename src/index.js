a3826872b8dcd342a23ddb3ff87ef557dda1a8d7
tri <tridesignnm@gmail.com>
2022-07-19 23:54:28 +0700

    doc du lieu tu form body voi method post dung express.urlencodes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ const { engine } = require("express-handlebars");
 const app = express();
 
 //http logger trong console log
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //static
 app.use(express.static(path.join(__dirname, "public")));
+
+//midle waee
+app.use(express.urlencoded({ extended: true })); //dung middle ware dde lay du lieu trong body gui tu client bang form, thang express tich hop san urlencoded, khong thoi phai dung body-parser
+app.use(express.json()); //dung middle ware dde lay du lieu gui tu client bang java jay php hay javascript
 
 //template engine
 app.engine("hbs", engine({ extname: ".hbs" }));
@@ -21,6 +25,15 @@ const port = 3001;
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.render("search");
 });
 
 app.listen(port, () => {
